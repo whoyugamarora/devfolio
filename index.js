671ca7492c20bbf101363c27dd797b7ccd7f433f
300177957 <yugam.arora@student.ufv.ca>
2022-11-16 13:37:47 -0800

    changed nav bar, remaining alignment of nav and adding transitions on navbar for social icons

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const togglebutton = document.getElementById("toggleimg");
+const togglebutton = document.getElementById("sun");
 const toggleicon = document.getElementById("darkmode1");
 const styleswap = document.getElementById("pagestyle");
 const homepic = document.getElementById("homepic");
 
 togglebutton.addEventListener('click', () => {
 
-    if(toggleicon.classList.contains('fa-toggle-off')) {
-        
-        toggleicon.classList.remove('fa-toggle-off');
-        toggleicon.classList.add('fa-toggle-on');
-        styleswap.setAttribute("href",'./darkindex.css');
+    if (toggleicon.classList.contains('fa-sun')) {
+
+        toggleicon.classList.remove('fa-sun');
+        toggleicon.classList.add('fa-moon');
+        styleswap.setAttribute("href", './darkindex.css');
         homepic.setAttribute("src", './pictures/personatdeskdark.svg');
 
     } else {
-        
-        toggleicon.classList.remove('fa-toggle-on');
-        toggleicon.classList.remove('--fa-inverse');
-        toggleicon.classList.add('fa-toggle-off');
 
-        styleswap.setAttribute("href",'./index.css');
+        toggleicon.classList.remove('fa-moon');
+        toggleicon.classList.add('fa-sun');
+
+        styleswap.setAttribute("href", './index.css');
         homepic.setAttribute("src", './pictures/personatdesk.svg');
     }
 
 });
+
+
+const hamburger = document.getElementById('hamburger');
+const socialicons = document.getElementById('social-icons');
+const hammenu = document.getElementById("hammenu");
+
+const menu = () => {
+    if (socialicons.style.display === 'none' && hammenu.classList.contains('fa-bars')) {
+        socialicons.style.display = 'flex';
+        hammenu.classList.remove('fa-bars');
+        hammenu.classList.add('fa-xmark');
+        socialicons.style.transform = 'translateX(0)';
+        
+    } else {
+        socialicons.style.display = 'none';
+        hammenu.classList.remove('fa-xmark');
+        hammenu.classList.add('fa-bars');
+        socialicons.style.transition = "all 2s";
+        hammenu.style.transition = "all 2s";
+    }
+};
